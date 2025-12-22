14cdd706664416f4c8944e2876589f87d67b570d
jubaid <jubaidhoss@gmail.com>
2025-12-21 23:54:59 -0500

    chore: initial commit - add frontend and backend scaffolding

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,18 @@
+
+
+
+export const getAllNotes = (req,res) => {
+    res.status(200).send('You just fetched the notes')
+}
+
+export const createNote = (req,res) => {
+    res.status(201).json({message: 'Note created successfully'})
+}
+
+export const updateNote = (req,res) => {  
+    res.status(200).send('Note updated successfully')
+}
+
+export const deleteNote = (req,res) => {
+    res.status(200).send('Note deleted successfully')
+}
